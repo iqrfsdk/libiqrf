65ad71bc3ae00f7338b9dcccd9cf5e28ced25c64
Ondřej Hujňák <ondrej.hujnak@iqrf.org>
2025-03-13 14:03:00 +0100

    GPIO: Filter out the unsortable chip names

--- src/gpio/GpioMap.cpp
+++ src/gpio/GpioMap.cpp
@@ -23,27 +23,24 @@ namespace iqrf::gpio {
 			}
 		}
 #else
-		std::vector<std::filesystem::path> chips;
+		std::vector<std::pair<unsigned long, std::filesystem::path>> chips;
 
 		// Load all GPIO chips
 		for (auto const &entry: std::filesystem::directory_iterator{GPIO_DIRECTORY}) {
 			if (entry.path().filename().string().find(GPIO_CHIP_PREFIX) == 0) {  // We care only about /dev/gpiochip* entries
 				if (gpiod::is_gpiochip_device(entry.path())) {  // Make sure it is GPIO
-					chips.emplace_back(entry.path());
+					try {
+						chips.emplace_back(
+							std::strtoul(entry.path().filename().string().substr(GPIO_CHIP_PREFIX_LEN)),
+							entry.path()
+						);
+					} catch (const std::exception &e) {}  // Ignore invalid chip names
 				}
 			}
 		}
 
 		// Sort the chips in numerical order by their ordinal number
-		std::sort(chips.begin(), chips.end(), [](const auto &a, const auto &b) {
-			try {
-				auto a_no = std::stoul(a.filename().string().substr(GPIO_CHIP_PREFIX_LEN));
-				auto b_no = std::stoul(b.filename().string().substr(GPIO_CHIP_PREFIX_LEN));
-				return a_no < b_no;
-			} catch (const std::exception &e) {  // Do not sort if error
-				return true;
-			}
-		});
+		std::sort(chips.begin(), chips.end(), [](const auto &a, const auto &b) { return a.first < b.first });
 
 		// Create the mapping according to the sorted chips
 		for (auto const &chip_path: chips) {

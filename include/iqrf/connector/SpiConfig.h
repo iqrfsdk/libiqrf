0213ff721f8785503fc9969aef8fb37066d64929
Ondřej Hujňák <ondrej.hujnak@iqrf.org>
2025-06-06 16:45:38 +0200

    SPI: WIP
    Signed-off-by: Roman Ondráček <mail@romanondracek.cz>

--- include/iqrf/connector/SpiConfig.h
+++ include/iqrf/connector/SpiConfig.h
@@ -11,6 +11,7 @@
 #pragma once
 
 #include <optional>
+#include <string>
 
 #include "iqrf/gpio/Gpio.h"
 
@@ -23,64 +24,55 @@ using iqrf::gpio::Gpio;
  */
 class SpiConfig {
  public:
-    /// GPIO to enable power supply to TR module
-    Gpio powerEnableGpio;
-    /// GPIO to enable function of SPI bus
-    Gpio spiEnableGpio;
-    /// GPIO to switch TR module to PGM mode
-    std::optional<Gpio> pgmSwitchGpio;
-    /// GPIO to enable function of UART bus (UART deactivated if set)
-    std::optional<Gpio> uartEnableGpio;
-    /// GPIO to enable function of I2C bus (I2C deactivated if set)
-    std::optional<Gpio> i2cEnableGpioPin;
+  /// System path to SPI device
+  std::string interface;
 
-    /**
-     * Constructs the minimal SPI connector configuration
-     * @param powerEnableGpio GPIO to enable power supply to TR module
-     * @param spiEnableGpio GPIO to enable function of SPI bus
-     */
-    SpiConfig(
-        const Gpio &powerEnableGpio,
-        const Gpio &spiEnableGpio
-    ) :
-        powerEnableGpio(powerEnableGpio),
-        spiEnableGpio(spiEnableGpio) {}
+  /// TR module reset during initialization (true by default)
+  std::bool trModuleReset;
 
-    /**
-     * Constructs the SPI connector configuration with programming mode
-     * @param powerEnableGpio GPIO to enable power supply to TR module
-     * @param spiEnableGpio GPIO to enable function of SPI bus
-     * @param pgmSwitchGpio GPIO to switch TR module to PGM mode
-     */
-    SpiConfig(
-        const Gpio &powerEnableGpio,
-        const Gpio &spiEnableGpio,
-        const Gpio &pgmSwitchGpio,
-    ) :
-        powerEnableGpio(powerEnableGpio),
-        spiEnableGpio(spiEnableGpio),
-        pgmSwitchGpio(pgmSwitchGpio) {}
+  /// GPIO to enable power supply to TR module
+  std::optional<Gpio> powerEnableGpio;
+  /// GPIO to enable function of SPI bus
+  std::optional<Gpio> spiEnableGpio;
+  /// GPIO to switch TR module to PGM mode
+  std::optional<Gpio> pgmSwitchGpio;
+  /// GPIO to enable function of UART bus (UART deactivated if set)
+  std::optional<Gpio> uartEnableGpio;
+  /// GPIO to enable function of I2C bus (I2C deactivated if set)
+  std::optional<Gpio> i2cEnableGpioPin;
 
-    /**
-     * Constructs the full SPI connector configuration
-     * @param powerEnableGpio GPIO to enable power supply to TR module
-     * @param spiEnableGpio GPIO to enable function of SPI bus
-     * @param pgmSwitchGpio GPIO to switch TR module to PGM mode
-     * @param uartEnableGpio GPIO to enable function of UART bus (UART deactivated if set)
-     * @param i2cEnableGpioPin GPIO to enable function of I2C bus (I2C deactivated if set)
-     */
-    SpiConfig(
-        const Gpio &powerEnableGpio,
-        const Gpio &spiEnableGpio,
-        const std::optional<Gpio> &pgmSwitchGpio,
-        const std::optional<Gpio> &uartEnableGpio,
-        const std::optional<Gpio> &i2cEnableGpioPin
-    ) :
+  /**
+   * Constructs the minimal SPI connector configuration
+   * @param interface is system path to an SPI device
+   * @param resetTr whether the TR module should be reset during initialization
+   */
+  SpiConfig(const std::string interface, const std::bool resetTr = true)
+      : interface(interface), trModuleReset(resetTr) {}
+
+  /**
+   * Constructs the full SPI connector configuration
+   * @param interface is system path to an SPI device
+   * @param powerEnableGpio GPIO to enable power supply to TR module
+   * @param spiEnableGpio GPIO to enable function of SPI bus
+   * @param pgmSwitchGpio GPIO to switch TR module to PGM mode
+   * @param uartEnableGpio GPIO to enable function of UART bus (UART deactivated if set)
+   * @param i2cEnableGpioPin GPIO to enable function of I2C bus (I2C deactivated if set)
+   * @param resetTr whether the TR module should be reset during initialization
+   */
+  SpiConfig(const std::string interface,
+            const std::optional<Gpio> &powerEnableGpio,
+            const std::optional<Gpio> &spiEnableGpio,
+            const std::optional<Gpio> &pgmSwitchGpio,
+            const std::optional<Gpio> &uartEnableGpio,
+            const std::optional<Gpio> &i2cEnableGpioPin,
+            const std::bool resetTr = true)
+      : interface(interface),
         powerEnableGpio(powerEnableGpio),
         spiEnableGpio(spiEnableGpio),
         pgmSwitchGpio(pgmSwitchGpio),
         uartEnableGpio(uartEnableGpio),
-        i2cEnableGpioPin(i2cEnableGpioPin) {}
+        i2cEnableGpioPin(i2cEnableGpioPin),
+        trModuleReset(resetTr) {}
 }
 
 }
